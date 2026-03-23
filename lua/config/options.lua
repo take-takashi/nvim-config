67c4fc93aec20b53fb7110fd8a88afe6709a4820
TAKE Takashi <puller.swipes1h@icloud.com>
2026-03-23 21:15:59 +0900

    feat(config): whichwrapで行またぎ移動を有効化
    変更内容:
    - lua/config/options.lua で `whichwrap` を設定し、カーソルの左右移動時に行をまたいで移動できるようにした
    - `h` `l` と左右キー、行頭・行末での移動対象をコメント付きで明示した
    
    背景:
    - 行端でカーソル移動が止まる既定挙動よりも、隣接行へ継続して移動できる方が編集操作に合っているため

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,9 @@ vim.opt.clipboard:append({"unnamedplus"})
 vim.opt.number = true
 -- カーソルラインを表示
 vim.opt.cursorline = true
+-- カーソルの左右移動で行をまたいで移動できるようにする
+-- <  : 行頭で左に移動すると前の行の末尾へ移動
+-- >  : 行末で右に移動すると次の行の先頭へ移動
+-- [ ]: ← → キーでも行またぎ移動を許可
+-- h l: ノーマルモードの h / l でも同様に行またぎ移動を許可
+vim.opt.whichwrap = "<,>,[,],h,l"
